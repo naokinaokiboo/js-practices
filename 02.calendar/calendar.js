1146522c9b0469729370c0526f0110edeb2b513c
naokinaokiboo <naokinaokiboo@gmail.com>
2023-09-16 17:46:03 +0900

    実装を自動テストができるように修正

--- 02.calendar/calendar.js
+++ 02.calendar/calendar.js
@@ -2,6 +2,41 @@
 
 import minimist from "minimist";
 
+const CALENDAR_ROWS = 6;
+
+export const generateCalendar = (year, month) => {
+  const weeks = new Array(CALENDAR_ROWS).fill("");
+  let weekIndex = 0;
+  weeks[weekIndex] = generateBlankOfFirstWeek(year, month);
+  for (const date of generateDatesOfMonth(year, month)) {
+    weeks[weekIndex] += ("  " + date.getDate().toString()).slice(-2) + " ";
+    if (date.getDay() === 6) {
+      weekIndex++;
+    }
+  }
+  weeks[CALENDAR_ROWS - 1] += "\n";
+
+  return [`      ${month}月 ${year}`, "日 月 火 水 木 金 土", ...weeks].join(
+    "\n"
+  );
+};
+
+const generateBlankOfFirstWeek = (year, month) => {
+  const firstDayOfWeek = new Date(year, month - 1, 1).getDay();
+  let blanks = "";
+  for (const blank of new Array(firstDayOfWeek).fill("   ")) {
+    blanks += blank;
+  }
+  return blanks;
+};
+
+const generateDatesOfMonth = (year, month) => {
+  const lastDay = new Date(year, month, 0).getDate();
+  return [...Array(lastDay)].map(
+    (_, day) => new Date(year, month - 1, day + 1)
+  );
+};
+
 const args = minimist(process.argv.slice(2));
 let year = args.y;
 let month = args.m;
@@ -11,35 +46,4 @@ if (year === undefined || month === undefined) {
   month = now.getMonth() + 1;
 }
 
-const firstDate = new Date(year, month - 1, 1);
-const firstDayOfWeek = firstDate.getDay();
-const lastDay = new Date(year, month, 0).getDate();
-
-console.log(`      ${month}月 ${year}`);
-console.log("日 月 火 水 木 金 土");
-
-for (const blank of new Array(firstDayOfWeek).fill("   ")) {
-  process.stdout.write(blank);
-}
-
-const dates = [...Array(lastDay)].map(
-  (_, day) => new Date(year, month - 1, day + 1)
-);
-
-let weekIndex = 0;
-for (const date of dates) {
-  let day = ("  " + date.getDate().toString() + " ").slice(-3);
-  process.stdout.write(day);
-  if (date.getDay() === 6) {
-    process.stdout.write("\n");
-    weekIndex++;
-  }
-}
-if (weekIndex === 4) {
-  console.log("");
-  weekIndex++;
-}
-
-if (weekIndex === 5) {
-  console.log("");
-}
+process.stdout.write(generateCalendar(year, month));
