d9b89bbdefa6659d2be41c00ddca3779b3f9c8eb
naokinaokiboo <naokinaokiboo@gmail.com>
2023-10-04 17:15:43 +0900

    SQLite3アクセス用のクラスを追加

---
+++ 04.class/SQLiteAdapter.js
@@ -0,0 +1,57 @@
+import sqlite3 from "sqlite3";
+
+export class SQLiteAdapter {
+  #db;
+
+  constructor() {
+    this.#db = new sqlite3.Database("./memo.sqlite3");
+  }
+
+  run(sql, ...params) {
+    return new Promise((resolve, reject) => {
+      this.#db.run(sql, params, function (err) {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(this);
+        }
+      });
+    });
+  }
+
+  get(sql, ...params) {
+    return new Promise((resolve, reject) => {
+      this.#db.get(sql, params, (err, row) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(row);
+        }
+      });
+    });
+  }
+
+  all(sql, ...params) {
+    return new Promise((resolve, reject) => {
+      this.#db.all(sql, params, (err, rows) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve(rows);
+        }
+      });
+    });
+  }
+
+  close() {
+    return new Promise((resolve, reject) => {
+      this.#db.close((err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+      });
+    });
+  }
+}
