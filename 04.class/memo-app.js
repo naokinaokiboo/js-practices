98caf0aea657e44dc465472c1438d3bf9c36a8c2
naokinaokiboo <naokinaokiboo@gmail.com>
2023-10-05 12:54:13 +0900

    main()関数をクラス化

--- 04.class/memo-app.js
+++ 04.class/memo-app.js
@@ -5,111 +5,126 @@ import enquirer from "enquirer";
 import { Memo } from "./memo.js";
 import { SQLiteAdapter } from "./sqlite-adapter.js";
 
-const main = async () => {
-  const adapter = new SQLiteAdapter();
-  try {
-    await prepareDB(adapter);
-    await receiveCommand(adapter);
-  } catch (err) {
-    if (err instanceof Error) {
-      console.error(err.message);
-    } else {
-      throw err;
+class MemoApp {
+  #adapter;
+  #optList;
+  #optReference;
+  #optDelete;
+
+  constructor() {
+    this.#adapter = new SQLiteAdapter();
+    const args = minimist(process.argv.slice(2));
+    this.#optList = args.l;
+    this.#optReference = args.r;
+    this.#optDelete = args.d;
+  }
+
+  async execute() {
+    try {
+      await this.#prepareDB();
+      await this.#executeCommand();
+    } catch (err) {
+      if (err instanceof Error) {
+        console.error(err.message);
+      } else {
+        throw err;
+      }
+    } finally {
+      await this.#adapter.close();
     }
-  } finally {
-    await adapter.close();
-  }
-};
-
-const prepareDB = (adapter) => {
-  return adapter.run(
-    "CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE, content TEXT NOT NULL)"
-  );
-};
-
-const receiveCommand = async (adapter) => {
-  const args = minimist(process.argv.slice(2));
-  if (!args.l && !args.r && !args.d) {
-    return saveMemo(adapter);
   }
 
-  const numOfMemos = await Memo.count(adapter);
-  if (numOfMemos === 0) {
-    console.log("No notes have been registered.");
-    return;
+  #prepareDB() {
+    return this.#adapter.run(
+      "CREATE TABLE IF NOT EXISTS memos (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL UNIQUE, content TEXT NOT NULL)"
+    );
   }
 
-  if (args.l) {
-    return showAllTitles(adapter);
-  } else if (args.r) {
-    return showMemo(adapter);
-  } else if (args.d) {
-    return deleteMemo(adapter);
+  async #executeCommand() {
+    const args = minimist(process.argv.slice(2));
+    if (!this.#optList && !this.#optReference && !this.#optDelete) {
+      return this.#saveMemo();
+    }
+
+    const numOfMemos = await Memo.count(this.#adapter);
+    if (numOfMemos === 0) {
+      console.log("No notes have been registered.");
+      return;
+    }
+
+    if (args.l) {
+      return this.#showAllTitles();
+    } else if (args.r) {
+      return this.#showMemo();
+    } else if (args.d) {
+      return this.#deleteMemo();
+    }
   }
-};
 
-const showMemo = async (adapter) => {
-  const selectedMemo = await selectMemo(adapter);
-  console.log(selectedMemo["title"]);
-  console.log(selectedMemo["content"]);
-};
-
-const showAllTitles = async (adapter) => {
-  const memos = await Memo.all(adapter);
-  for (const memo of memos) {
-    console.log(memo["title"]);
+  async #saveMemo() {
+    const lines = await this.#readlinePromise();
+    const memo = new Memo(lines[0], lines.slice(1).join("\n"));
+    return memo.save(this.#adapter);
   }
-};
 
-const deleteMemo = async (adapter) => {
-  const selectedMemo = await selectMemo(adapter);
-  return Memo.destroy(adapter, selectedMemo["id"]);
-};
+  #readlinePromise() {
+    const reader = readline.createInterface({
+      input: process.stdin,
+    });
 
-const selectMemo = async (adapter) => {
-  const memos = await Memo.all(adapter);
-  const questions = [
-    {
-      type: "select",
-      name: "value",
-      message: "Select the memo you want to display.",
-      choices: memos.map((memo) => {
-        return {
-          name: memo.title,
-          message: memo.title,
-          value: { id: memo.id, title: memo.title, content: memo.content },
-        };
-      }),
-      result() {
-        return this.focused.value;
+    const lines = [];
+    return new Promise((resolve) => {
+      reader.on("line", (line) => {
+        lines.push(line);
+      });
+      reader.on("close", () => {
+        resolve(lines);
+      });
+    });
+  }
+
+  async #showAllTitles() {
+    const memos = await Memo.all(this.#adapter);
+    for (const memo of memos) {
+      console.log(memo["title"]);
+    }
+  }
+
+  async #showMemo() {
+    const selectedMemo = await this.#selectMemo();
+    console.log(selectedMemo["title"]);
+    console.log(selectedMemo["content"]);
+  }
+
+  async #deleteMemo() {
+    const selectedMemo = await this.#selectMemo();
+    return Memo.destroy(this.#adapter, selectedMemo["id"]);
+  }
+
+  async #selectMemo() {
+    const memos = await Memo.all(this.#adapter);
+    const questions = [
+      {
+        type: "select",
+        name: "value",
+        message: "Select the memo you want to display.",
+        choices: memos.map((memo) => {
+          return {
+            name: memo.title,
+            message: memo.title,
+            value: { id: memo.id, title: memo.title, content: memo.content },
+          };
+        }),
+        result() {
+          return this.focused.value;
+        },
       },
-    },
-  ];
+    ];
 
-  const answer = await enquirer.prompt(questions);
-  return answer["value"];
-};
+    const answer = await enquirer.prompt(questions);
+    return answer["value"];
+  }
+}
 
-const saveMemo = async (adapter) => {
-  const lines = await readlinePromise();
-  const memo = new Memo(lines[0], lines.slice(1).join("\n"));
-  return memo.save(adapter);
-};
-
-const readlinePromise = () => {
-  const reader = readline.createInterface({
-    input: process.stdin,
-  });
-
-  const lines = [];
-  return new Promise((resolve) => {
-    reader.on("line", (line) => {
-      lines.push(line);
-    });
-    reader.on("close", () => {
-      resolve(lines);
-    });
-  });
-};
-
-main();
+const memoApp = new MemoApp();
+memoApp.execute();
